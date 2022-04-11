187f83b5b8a1ebc3af14c53ff56f93073eed52be
ian-otieno <ian.otieno@student.moringaschool.com>
2022-04-11 17:33:50 +0300

    add post and update form to forms.py

--- myawwards/forms.py
+++ myawwards/forms.py
@@ -12,4 +12,19 @@ class SignupForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
+class PostForm(forms.ModelForm):
+    photo = ImageField(label='')
+
+    class Meta:
+        model = Post
+        fields = ('photo', 'title', 'url', 'description', 'technologies',)
+
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
 
