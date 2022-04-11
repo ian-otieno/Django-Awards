6ae13fb97d3c4760f03e223b4f1d70551fe23d6e
ian-otieno <ian.otieno@student.moringaschool.com>
2022-04-11 16:42:56 +0300

    add class testpost to test.py

--- myawwards/models.py
+++ myawwards/models.py
@@ -4,6 +4,7 @@ from cloudinary.models import CloudinaryField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 import datetime as dt
+from django.forms import ImageField
 
 # Create your models here.
 class Profile(models.Model):
