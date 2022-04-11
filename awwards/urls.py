6d45c240d1c90d746432e9657b542e80afdd0553
ian-otieno <ian.otieno@student.moringaschool.com>
2022-04-11 23:34:54 +0300

    edit urls.py

--- awwards/urls.py
+++ awwards/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path,include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myawwards.urls'))
+    path('',include('myawwards.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', views.LogoutView.as_view(), {'next_page':'/accounts/register/'}),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
