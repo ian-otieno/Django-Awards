17e643029efaaccc28de003d23b08787df3310e0
ian-otieno <ian.otieno@student.moringaschool.com>
2022-04-11 16:54:07 +0300

    add class post serializer to serializer.py

--- myawwards/serializers.py
+++ myawwards/serializers.py
@@ -8,17 +8,9 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = ['name', 'profile_picture', 'bio', 'location', 'contact']
 
-
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'title', 'url', 'description', 'technologies', 'photo', 'date', 'user']
 
 
-class UserSerializer(serializers.ModelSerializer):
-    profile = ProfileSerializer(read_only=True)
-    posts = PostSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = User
-        fields = ['id', 'url', 'username', 'profile', 'posts']
