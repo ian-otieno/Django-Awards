c89e2e10dedc929812cec4507177d6a278eb41b3
ian-otieno <ian.otieno@student.moringaschool.com>
2022-04-12 07:06:24 +0300

    add.env file

--- myawwards/admin.py
+++ myawwards/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import UserProfile, Project, Rating
+
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(Project)
 admin.site.register(Rating)
+
 
